2ad9220fc70867201402731e1516ecf480e73516
Po-Kai Huang <000429977@student.vcc.ca>
2021-10-13 15:58:16 -0700

    Updated SignUpScreen UI

--- app/src/main/java/com/example/foodvillage2205/view/theme/Color.kt
+++ app/src/main/java/com/example/foodvillage2205/view/theme/Color.kt
@@ -2,18 +2,9 @@ package com.example.foodvillage2205.view.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
 val PrimaryColor = Color(32, 106, 93)
 val SecondaryColor = Color(14, 149, 119)
 val ThirdColor = Color(255, 204, 41)
 val FourthColor = Color(245, 134, 52)
 
-val White = Color(0xfffffff)
-val BackgroundColor = Color(251, 250, 225)
-
-val TextDark  = Color(0xF8232323)
-val TextLight = Color(0x7F232323)
+val White = Color(0xFFF7F7F7)
