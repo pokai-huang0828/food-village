b0215295c5c91d516c792c3d90963a42f87f5122
Po-Kai Huang <000429977@student.vcc.ca>
2021-10-19 13:20:38 -0700

    Updated Main Page UI

--- app/src/main/java/com/example/foodvillage2205/view/screens/MainScreen.kt
+++ app/src/main/java/com/example/foodvillage2205/view/screens/MainScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,9 +26,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.foodvillage2205.R
-import com.example.foodvillage2205.view.navigation.Route
-import com.example.foodvillage2205.util.ReadMoreModifier
-import com.example.foodvillage2205.view.theme.FourthColor
 import com.example.foodvillage2205.view.theme.PrimaryColor
 import com.example.foodvillage2205.view.theme.SecondaryColor
 import com.example.foodvillage2205.view.theme.ThirdColor
@@ -33,31 +33,9 @@ import com.example.foodvillage2205.view.theme.ThirdColor
 @Composable
 fun MainScreen(navController: NavController, signOut: () -> Unit) {
     Scaffold(
+        topBar = { TopBar(signOut = signOut) },
         content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(rememberScrollState())
-            ) {
-                TopBar(signOut = signOut)
-                SearchBar()
-                Spacer(modifier = Modifier.heightIn(20.dp))
-                FoodItem(
-                    image = painterResource(id = R.drawable.rice_snacks),
-                    header = "Rice Snacks",
-                    navController = navController,
-                    itemDescription = "Our Rice Crisps come in a delicious variety of flavours to satisfy your snack cravings any \ntime of day."
-                )
-                Spacer(modifier = Modifier.heightIn(20.dp))
-                FoodItem(
-                    image = painterResource(id = R.drawable.spaghetti),
-                    header = "Spaghetti",
-                    navController = navController,
-                    itemDescription = "It is the quintessential Italian pasta. It is long \n- like a string " +
-                            "- round in cross-section and made from durum wheat semolina."
-                )
-                Spacer(modifier = Modifier.heightIn(100.dp))
-            }
+            FoodList(navController)
         },
         floatingActionButton = {
             FloatingActionButton(
@@ -66,7 +44,8 @@ fun MainScreen(navController: NavController, signOut: () -> Unit) {
                 },
                 backgroundColor = SecondaryColor,
                 contentColor = Color.White,
-                modifier = Modifier.size(79.dp)
+                modifier = Modifier.size(85.dp),
+
             ) {
                 Column(
                     verticalArrangement = Arrangement.Center,
@@ -75,18 +54,16 @@ fun MainScreen(navController: NavController, signOut: () -> Unit) {
                 ) {
                     Icon(
                         imageVector = Icons.Filled.Add,
-                        contentDescription = "FAB"
+                        contentDescription = "FAB",
+                        modifier = Modifier.size(39.dp)
                     )
                     Text(
                         text = "Donate",
                         fontSize = 18.sp,
-                        fontFamily = RobotoSlab
                     )
                 }
-
             }
         }
-
     )
 }
 
@@ -95,107 +72,132 @@ fun TopBar(
     modifier: Modifier = Modifier,
     signOut: () -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .background(SecondaryColor)
-            .fillMaxWidth()
-            .height(60.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(id = R.drawable.food_village_logo_1),
-            contentDescription = "logo",
-            alignment = Alignment.CenterStart,
-            modifier = Modifier
-                .size(79.dp)
-                .padding(2.dp)
-        )
-
-        Image(
-            painter = painterResource(id = R.drawable.logo_text),
-            contentDescription = "logo",
-            alignment = Alignment.Center,
-            modifier = Modifier
-                .size(190.dp)
-                .padding(2.dp)
-        )
+    Column() {
         Row(
             modifier = Modifier
                 .background(SecondaryColor)
-                .height(60.dp)
-                .padding(2.dp),
-            horizontalArrangement = Arrangement.End,
+                .fillMaxWidth()
+                .height(60.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                imageVector = Icons.Filled.AccountCircle,
-                contentDescription = "Account",
-                tint = ThirdColor,
-                modifier = Modifier.size(47.dp)
+            Image(
+                painter = painterResource(id = R.drawable.food_village_logo_1),
+                contentDescription = "logo",
+                alignment = Alignment.CenterStart,
+                modifier = Modifier
+                    .size(80.dp)
+                    .padding(3.dp)
             )
 
-            Spacer(modifier = modifier.padding(5.dp))
-            
-            Icon(
-                imageVector = Icons.Filled.Logout,
-                contentDescription = "Messages",
-                tint = ThirdColor,
+            Image(
+                painter = painterResource(id = R.drawable.logo_text),
+                contentDescription = "logo",
+                alignment = Alignment.Center,
                 modifier = Modifier
-                    .size(47.dp)
-                    .clickable { signOut() }
+                    .size(190.dp)
+                    .padding(2.dp)
             )
+
+            Row(
+                modifier = Modifier
+                    .background(SecondaryColor)
+                    .height(60.dp)
+                    .padding(2.dp),
+                horizontalArrangement = Arrangement.End,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Account",
+                    tint = ThirdColor,
+                    modifier = Modifier
+                        .size(47.dp)
+                        .clickable {  }
+                )
+
+                Spacer(modifier = modifier.padding(5.dp))
+
+                Icon(
+                    imageVector = Icons.Filled.Logout,
+                    contentDescription = "Messages",
+                    tint = ThirdColor,
+                    modifier = Modifier
+                        .size(47.dp)
+                        .clickable { signOut() }
+                )
+            }
         }
+        SearchBar()
     }
 }
 
 @Composable
 fun SearchBar() {
+    var searchText by remember { mutableStateOf("") }
     Surface(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth(),
         color = Color.White,
-        elevation = 5.dp,
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth(0.9f)
                 .padding(8.dp)
         ) {
-
-
-        }
-    }
-}
-
-@Composable
-fun SearchRow(
-    modifier: Modifier = Modifier
-) {
-    var searchText by remember { mutableStateOf("") }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 10.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Box {
-            Icon(
-                imageVector = Icons.Filled.Search,
-                contentDescription = "Account",
-                tint = Color.Green,
-                modifier = Modifier
-                    .size(47.dp)
-                    .padding(top = 7.dp, start = 5.dp)
-            )
             TextField(
                 value = searchText,
                 onValueChange = { searchText = it },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .clip(RoundedCornerShape(10.dp))
+                    .clip(RoundedCornerShape(10.dp)),
+                maxLines = 1,
+                label = {
+                    Text(text = "Search")
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "email",
+                        tint = SecondaryColor,
+                    )
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = ThirdColor,
+                    focusedLabelColor = SecondaryColor,
+                    unfocusedBorderColor = SecondaryColor
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Search,
+                ),
             )
         }
+    }
+}
+
+@Composable
+fun FoodList(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .padding(10.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        FoodItem(
+            image = painterResource(id = R.drawable.rice_snacks),
+            header = "Rice Snacks",
+            navController = navController,
+            itemDescription = "Our Rice Crisps come in a delicious variety of flavours to satisfy your snack cravings any \ntime of day."
+        )
+        FoodItem(
+            image = painterResource(id = R.drawable.spaghetti),
+            header = "Spaghetti",
+            navController = navController,
+            itemDescription = "It is the quintessential Italian pasta. It is long \n- like a string " +
+                    "- round in cross-section and made from durum wheat semolina."
+        )
+
+        Spacer(modifier = Modifier.padding(bottom = 80.dp))
     }
 }
 
@@ -210,43 +212,48 @@ fun FoodItem(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 10.dp),
+            .padding(10.dp),
         horizontalAlignment = Alignment.Start
     ) {
         Image(
             painter = image,
             contentDescription = header,
             modifier = Modifier
-                .size(width = 250.dp, height = 150.dp)
+                .fillMaxWidth(0.9f)
+                .height(160.dp)
                 .clip(RoundedCornerShape(15.dp)),
             contentScale = ContentScale.Crop,
-
+            alignment = Alignment.Center
             )
         Text(
             text = "$header:",
             fontWeight = FontWeight.Bold,
             fontSize = 22.sp,
             fontStyle = FontStyle.Italic,
-            modifier = Modifier.padding(top = 10.dp, bottom = 5.dp)
+            modifier = Modifier.padding(top = 10.dp, bottom = 5.dp),
+            color = PrimaryColor,
         )
         Text(
             text = itemDescription,
             fontWeight = FontWeight.Normal,
             fontSize = 17.sp,
-            fontStyle = FontStyle.Italic
         )
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.read_more_btn),
-                contentDescription = "Read more btn",
-                modifier = ReadMoreModifier(),
-                contentScale = ContentScale.Crop
-            )
-            TextButton(
-                onClick = {  },
-                modifier = ReadMoreModifier()
+
+        Button(
+            onClick = {  },
+            colors = ButtonDefaults.buttonColors(backgroundColor = SecondaryColor),
+            modifier = Modifier
+                .padding(top = 15.dp)
+                .width(120.dp)
+                .height(35.dp),
             ) {
-            }
+            Text(
+                fontFamily = RobotoSlab,
+                color = Color.White,
+                text = "Read More",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W900,
+            )
         }
     }
 }
